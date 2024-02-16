b3da47dfb208626230292cd0f8cf45ea549616b3
Samuel25100 <samuelgselassie1@gmail.com>
2024-02-16 13:27:43 +0300

    add 0-bubble_sort.c in sam_test dircetory

---
+++ sam_test/0-bubble_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+/**
+ * bubble_sort - bubble sort the input array
+ * @array: the array to be sorted
+ * @size: size of the array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	long unsigned int i, j, flag;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+	flag = 0;
+		for (j = 0; j < (size - i - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+			swap(array, j, j + 1);
+			flag = 1;
+			print_array(array, size);
+			}
+		}
+	if (flag == 0)
+		break;
+	}
+}
+/**
+ * sawp - swap two value
+ * @array: pointer to the array
+ * @index0: index to value of an array
+ * @index1: index to value of an other array
+ * Return: void
+ */ 
+void swap(int *array, int index0, int index1)
+{
+	int temp;
+
+	temp = array[index0];
+	array[index0] = array[index1];
+	array[index1] = temp;
+}
