e654ae0c85a95d99c9bbe95f3acb24c255411aa8
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-02-20 11:44:56 +0300

    task

---
+++ 100-shell_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+ * shell_sort - sorts elements using shell sort algo
+ * @array: address of the array
+ * @size: size of the array
+ *
+ */
+void shell_sort(int *array, size_t size)
+{
+	int gap, temp, j, i;
+	int size_ = size;
+
+	gap = 1;
+
+	while (gap < size_ / 3)
+	{
+		gap = 3 * gap + 1;
+	}
+	while (gap > 0)
+	{
+		for (j = gap; j < size_; j++)
+		{
+			for (i = j - gap; i >= 0; i = i - gap)
+			{
+				if (array[i + gap] < array[i])
+				{
+					temp = array[i];
+					array[i] = array[i + gap];
+					array[i + gap] = temp;
+				}
+				else
+					break;
+			}
+		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
+}
---
+++ 100-shell_sort_1.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+/**
+ * shell_sort - sort elements using shell sort algorithm
+ * @array: address of the array
+ * @size: size of the array
+ *
+ */
+void shell_sort(int *array, size_t size)
+{
+	int gap, temp, j, i;
+	int size_ = size;
+
+	gap = 1;
+	while (gap < size_ / 3)
+	{
+		gap = gap * 3 + 1;
+	}
+	while (gap > 0)
+	{
+		for (j = gap; j < size_; j++)
+		{
+			i = 0;
+			while (i < size_)
+			{
+				if (i + gap < size_ && array[i] > array[i + gap])
+				{
+					temp = array[i];
+					array[i] = array[i + gap];
+					array[i + gap] = temp;
+					if (i - gap >= 0 && array[i] > array[i - gap])
+					{
+						temp = array[i];
+						array[i] = array[i - gap];
+						array[i - gap] = temp;
+					}
+				}
+				i++;
+			}
+		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
+}
